9597170a788ca3e5eeb32145114795007e2a50a8
FreshCrash <ikingize@gmail.com>
2025-04-07 18:52:52 +0300

    GameView

---
+++ app/src/main/java/com/example/recolor/GameView.java
@@ -0,0 +1,63 @@
+package com.example.recolor;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+public class GameView extends View {
+
+    private int viewWidth;
+    private int viewHeight;
+    float x1, y1, x2, y2;
+    Tile[][] field;
+    public GameView(Context context) {
+        super(context);
+    }
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        viewWidth = w;
+        viewHeight = h;
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        switch (event.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                y1 = event.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                y2 = event.getY();
+                float magnitudeX = Math.abs(x1 - x2);
+                float magnitudeY = Math.abs(y1 - y2);
+                float totalMagnitude = (float) Math.sqrt(magnitudeX * magnitudeX + magnitudeY * magnitudeY);
+                if(totalMagnitude >= 10){
+                    if(magnitudeX > magnitudeY){
+                        if(x1 - x2 > 0){
+                            Toast.makeText(this.getContext(), "Left", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(this.getContext(), "Right", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else {
+                        if(y1 - y2 > 0){
+                            Toast.makeText(this.getContext(), "Up", Toast.LENGTH_SHORT).show();
+                        }
+                        else {
+                            Toast.makeText(this.getContext(), "Down", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+        }
+        return false;
+    }
+}
--- app/src/main/java/com/example/recolor/MainActivity.java
+++ app/src/main/java/com/example/recolor/MainActivity.java
@@ -21,12 +21,12 @@ public class MainActivity extends AppCompatActivity {
     GestureDetector gestureDetector;
 
     Tile[][] field;
-    float x1, y1, x2, y2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(new GameView(this));
         this.gestureDetector = new GestureDetector(MainActivity.this, this);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -35,40 +35,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
-    @Override
-    public boolean onTouchEvent(MotionEvent event){
-        switch (event.getAction()){
-            case MotionEvent.ACTION_DOWN:
-                x1 = event.getX();
-                y1 = event.getY();
-                break;
-            case MotionEvent.ACTION_UP:
-                x2 = event.getX();
-                y2 = event.getY();
-                float magnitudeX = Math.abs(x1 - x2);
-                float magnitudeY = Math.abs(y1 - y2);
-                float totalMagnitude = (float) Math.sqrt(magnitudeX * magnitudeX + magnitudeY * magnitudeY);
-                if(totalMagnitude >= 10){
-                    if(magnitudeX > magnitudeY){
-                        if(x1 - x2 > 0){
-                            Toast.makeText(this, "Left", Toast.LENGTH_SHORT).show();
-                        }
-                        else {
-                            Toast.makeText(this, "Right", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                    else {
-                        if(y1 - y2 > 0){
-                            Toast.makeText(this, "Up", Toast.LENGTH_SHORT).show();
-                        }
-                        else {
-                            Toast.makeText(this, "Down", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                }
-        }
-        return false;
-    }
+
     void loadLevel(File file) throws IOException {
         CSVReader reader = new CSVReader(new FileReader(file));
         List myEntries = reader.readAll();
